e13993acffefc5b34b8b9ecd8596396f44054eb5
Giacomo <giacomo.madella@unibo.it>
2024-09-16 16:25:11 +0200

    v0.4.5 fixed matrix

--- assets/scripts/404.js
+++ assets/scripts/404.js
@@ -1,5 +1,5 @@
+let starContainer = document.querySelector(".stars");
 
-let starContainer = document.getElementById("starsID");
 for (let i = 0; i < 100; i++) {
   starContainer.innerHTML += `<div class="star"></div>`;
 }
